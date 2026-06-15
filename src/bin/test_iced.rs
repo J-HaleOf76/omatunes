f650cbc96d754e6366132fd69f270268f14c0378
Balthazzahr <Balthazzahr@users.noreply.github.com>
2026-06-15 14:36:24 +1000

    Auto-commit: local modifications sync

--- src/bin/test_iced.rs
+++ src/bin/test_iced.rs
@@ -1,6 +1,26 @@
 use iced::widget::text_editor;
 
-fn main() {
-    let _content = text_editor::Content::new();
-    println!("Compiled!");
+#[derive(Debug, Clone)]
+enum Message {
+    LyricsAction(text_editor::Action),
 }
+
+struct AppState {
+    lyrics_content: text_editor::Content,
+}
+
+fn update(state: &mut AppState, message: Message) {
+    match message {
+        Message::LyricsAction(action) => {
+            state.lyrics_content.perform(action);
+        }
+    }
+}
+
+fn view(state: &AppState) -> iced::Element<'_, Message> {
+    text_editor(&state.lyrics_content)
+        .on_action(Message::LyricsAction)
+        .into()
+}
+
+fn main() {}
