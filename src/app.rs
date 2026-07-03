0be09c0186f5af7e398f32094d256b0257c91568
Balthazzahr <Balthazzahr@users.noreply.github.com>
2026-07-03 17:34:41 +1000

    Auto-commit: local modifications sync

--- src/app.rs
+++ src/app.rs
@@ -3943,11 +3943,6 @@ impl AppState {
                 Task::none()
             }
 
-            Message::CursorMoved(x) => {
-                self.cursor_x = x;
-                Task::none()
-            }
-
             Message::ColumnHeaderDragStart(col) => {
                 self.dragging_column_header = Some(col);
                 self.column_drag_moved = false;
@@ -3989,48 +3984,6 @@ impl AppState {
                 }
                 self.dragging_column_header = None;
                 self.column_drag_moved = false;
-                Task::none()
-            }
-
-            Message::ColumnWidthResizeStart(col, x) => {
-                self.resizing_column = Some(col);
-                self.resize_column_start_x = x;
-                Task::none()
-            }
-
-            Message::ColumnWidthResizeDrag(x) => {
-                if let Some(col) = self.resizing_column {
-                    let delta = x - self.resize_column_start_x;
-                    self.resize_column_start_x = x;
-                    crate::db::write(|db| {
-                        let current_px = db.column_widths.get(&col).copied().unwrap_or_else(|| {
-                            match col {
-                                crate::db::TableColumn::TrackNumber => 30.0,
-                                crate::db::TableColumn::Year => 50.0,
-                                crate::db::TableColumn::DiscNumber => 50.0,
-                                crate::db::TableColumn::Duration => 80.0,
-                                crate::db::TableColumn::Plays => 40.0,
-                                crate::db::TableColumn::Liked => 40.0,
-                                _ => 160.0,
-                            }
-                        });
-                        let new_px = (current_px + delta).max(24.0);
-                        db.column_widths.insert(col, new_px);
-                    });
-                }
-                Task::none()
-            }
-
-            Message::ColumnWidthResizeEnd => {
-                self.resizing_column = None;
-                Task::none()
-            }
-
-            Message::ResetColumnWidths => {
-                crate::db::write(|db| {
-                    db.column_widths.clear();
-                });
-                self.show_context_menu = None;
                 Task::none()
             }
         }
