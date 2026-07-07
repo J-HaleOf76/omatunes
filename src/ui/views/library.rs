21c588841a0a7941c41facdd3c6a508b8055ffa2
Balthazzahr <Balthazzahr@users.noreply.github.com>
2026-07-08 08:31:54 +1000

    Auto-commit: local modifications sync

--- src/ui/views/library.rs
+++ src/ui/views/library.rs
@@ -1051,7 +1051,7 @@ fn track_list_view(state: &AppState) -> Element<'_, Message> {
                     crate::db::GroupBy::Album => track.album.clone(),
                     crate::db::GroupBy::Artist => track.artist.clone(),
                     crate::db::GroupBy::Genre => track.genre.clone(),
-                    crate::db::GroupBy::Year => track.year.to_string(),
+                    crate::db::GroupBy::Year => track.year.map(|y| y.to_string()).unwrap_or_default(),
                     crate::db::GroupBy::None => unreachable!(),
                 };
                 if let Some(last) = groups.last_mut() {
