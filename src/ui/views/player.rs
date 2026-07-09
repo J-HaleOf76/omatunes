2a12d2f697ab817ff71d462cbdb5e7d29bb21eb8
Balthazzahr <Balthazzahr@users.noreply.github.com>
2026-07-09 11:31:18 +1000

    Auto-commit: local modifications sync

--- src/ui/views/player.rs
+++ src/ui/views/player.rs
@@ -388,6 +388,7 @@ pub fn right_panel(state: &AppState) -> Option<Element<'_, Message>> {
             .center_y(Length::Fill)
             .into()
         }
+        crate::app::RightPanelTab::Statistics |
         crate::app::RightPanelTab::Lyrics => {
             let display_track = if !matches!(state.playback_state, crate::audio::PlaybackState::Stopped) {
                 state.current_track.as_ref()
